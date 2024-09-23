2186f91cd8fa8a07b73a6307a99727337b7eb396
semaroman <106012219+semaroman@users.noreply.github.com>
2024-09-23 22:05:14 +0300

    Update Repository.java

--- src/main/java/ru/netology/repository/Repository.java
+++ src/main/java/ru/netology/repository/Repository.java
@@ -1,1 +1,30 @@
+package ru.netology.repository;
 
+import person.Person;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Repository;
+
+import javax.lang.model.element.Name;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+public interface Repository extends JpaRepository<Persons, Integer> {
+    
+    @Query("select p from Person p where p.cityOfLiving = :cityofLiving")
+    List<Persons> findByCityOfLiving(@Param("cityOfLiving") String cityOfLiving);
+
+    @Query("select p from Person p where p.age < :age")
+    List<Persons> findByAgeLessThan(@Param("age") int age);
+
+    @Query("select p from Person p where p.name = :name and p.surname = :surname")
+
+    Optional<Persons> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
+    
+}
