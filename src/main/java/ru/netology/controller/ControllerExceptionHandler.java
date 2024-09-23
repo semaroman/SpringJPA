dea78caece6c78e223c14b239d49fb37b9074f1d
semaroman <106012219+semaroman@users.noreply.github.com>
2024-09-23 21:01:42 +0300

    Update ControllerExceptionHandler.java

--- src/main/java/ru/netology/controller/ControllerExceptionHandler.java
+++ src/main/java/ru/netology/controller/ControllerExceptionHandler.java
@@ -1,1 +1,22 @@
+package controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    String handleBindException(BindException e) {
+        return e.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    String handleRuntimeException(RuntimeException e) {
+        return e.getMessage();
+    }
+}
